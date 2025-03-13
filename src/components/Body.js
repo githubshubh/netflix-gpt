ea8ee7d1fee8568d68f645bcab7319568a21d5cb
Shubham Singh <68952643+githubshubh@users.noreply.github.com>
2025-03-13 22:22:19 +0530

    redux store and authentication logic

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,25 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { onAuthStateChanged } from "firebase/auth";
+import {auth} from '../utils/firebase';
+import {useDispatch} from 'react-redux';
+import {addUser, removeUser} from '../store/userSlice'; 
 
 const Body = () => {
 
+    const dispatch = useDispatch();
+
     const appRouter = createBrowserRouter([
         {
-            path:'/',
-            element:<Login/>
+            path: '/',
+            element: <Login />
         },
         {
-            path:'/browse',
-            element:<Browse/>
+            path: '/browse',
+            element: <Browse />
         }
     ])
-  return (
-    <div>
-        <RouterProvider router={appRouter}/>
-    </div>
-  )
+
+    useEffect(() => {
+
+
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                const {uid, displayName, email,} = user;
+                dispatch(addUser({uid:uid, displayName:displayName, email:email}));
+            } else {
+                dispatch(removeUser());               
+            }
+        })
+    }, [])
+
+    return (
+        <div>
+            <RouterProvider router={appRouter} />
+        </div>
+    )
 }
 
 export default Body;
